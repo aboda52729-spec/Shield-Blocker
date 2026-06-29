87fdfdfc41e9089ba6eb05d16cba5ddbf5e1b0c2
aboda52729-spec <aboda52729@gmail.com>
2026-06-29 11:31:29 +0200

    build: disable lint checks on release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,11 @@ android {
     buildConfig = true
   }
   testOptions { unitTests { isIncludeAndroidResources = true } }
+
+  lint {
+    checkReleaseBuilds = false
+    abortOnError = false
+  }
 }
 
 // Configure the Secrets Gradle Plugin to use .env and .env.example files
